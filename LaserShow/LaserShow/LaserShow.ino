094a3f7b9223083fa102fa5a046d45387b57605a
elotronico <eloy_tronic@hotmail.es>
2017-08-09 00:04:25 +0200

    limpiando codigo
    seguimos eliminado mas codigo

--- LaserShow/LaserShow/LaserShow.ino
+++ LaserShow/LaserShow/LaserShow.ino
@@ -4,36 +4,33 @@
 // Copyright 2016 Florian Link (at) gmx.de
 
 #include "Laser.h"
-#include "Drawing.h"
-#include "Cube.h"
-#include "Objects.h"
-#include "Logo.h"
+
 
 // Create laser instance (with laser pointer connected to digital pin 5)
-Laser laser(5);
+Laser miLaser(5);
 
 void setup()
 {
-  laser.init();
+  miLaser.init();
 }
 
 // draw a circle using sin/cos
 void circle() {
   const int scale = 12;
-  laser.sendto(SIN(0)/scale, COS(0)/scale);
-  laser.on();
+  miLaser.sendto(SIN(0)/scale, COS(0)/scale);
+  miLaser.on();
   for (int r = 5;r<=360;r+=5)
   {
-    laser.sendto(SIN(r)/scale, COS(r)/scale);
+    miLaser.sendto(SIN(r)/scale, COS(r)/scale);
   }
-  laser.off();
+  miLaser.off();
 }
 
 
 
 void loop() {
 
-  rotateCube(400);
+  circle(); 
 
 
 //  drawObjects();
